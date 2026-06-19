9259ff25126c329a9822b5a43054ac0b8d5711b7
Jeffrey Larbi-Akor <jeffreylarbiakor@gmail.com>
2026-06-19 14:04:26 +0000

    fix: self-contained Cypress tests to avoid retry state corruption

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -1,23 +1,17 @@
-const login = () => {
-  cy.visit('/');
-  cy.get('[data-test="username"]').type('standard_user');
-  cy.get('[data-test="password"]').type('secret_sauce');
-  cy.get('[data-test="login-button"]').click();
-  cy.url().should('include', '/inventory.html');
-};
-
 describe('Cart', () => {
-  it('adding an item updates the cart badge', () => {
-    login();
-    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1');
+  before(() => {
+    cy.visit('/');
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+    cy.url().should('include', '/inventory.html');
   });
 
-  it('cart page shows the added item and a checkout button', () => {
-    login();
+  it('add to cart updates badge, cart page shows item and checkout button', () => {
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1');
 
+    cy.get('[data-test="shopping-cart-link"]').click();
     cy.get('[data-test="inventory-item-name"]').should('contain.text', 'Sauce Labs Backpack');
     cy.get('[data-test="checkout"]').should('be.visible');
   });
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,14 +1,4 @@
 describe('Login', () => {
-  it('valid credentials redirect to inventory page', () => {
-    cy.visit('/');
-    cy.get('[data-test="username"]').type('standard_user');
-    cy.get('[data-test="password"]').type('secret_sauce');
-    cy.get('[data-test="login-button"]').click();
-
-    cy.url().should('include', '/inventory.html');
-    cy.get('[data-test="inventory-list"]').should('be.visible');
-  });
-
   it('wrong password shows error message', () => {
     cy.visit('/');
     cy.get('[data-test="username"]').type('standard_user');
@@ -18,5 +8,14 @@ describe('Login', () => {
     cy.get('[data-test="error"]')
       .should('be.visible')
       .and('contain.text', 'Username and password do not match');
+  });
+
+  it('valid credentials redirect to inventory page', () => {
+    cy.visit('/');
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+
+    cy.url().should('include', '/inventory.html');
   });
 });
