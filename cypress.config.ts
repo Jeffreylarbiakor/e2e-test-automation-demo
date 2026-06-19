c5a2b542ae51dcfc6680b243b6f55e8807a9fb88
Jeffrey Larbi-Akor <jeffreylarbiakor@gmail.com>
2026-06-19 13:40:46 +0000

    fix: remove shared beforeEach in login spec, increase pageLoadTimeout and add retries for CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     supportFile: false,
+    pageLoadTimeout: 120000,
+    retries: { runMode: 2, openMode: 0 },
   },
 });
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,9 +1,6 @@
 describe('Login', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
   it('valid credentials redirect to inventory page', () => {
+    cy.visit('/');
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
@@ -13,6 +10,7 @@ describe('Login', () => {
   });
 
   it('wrong password shows error message', () => {
+    cy.visit('/');
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('wrong_password');
     cy.get('[data-test="login-button"]').click();
